0ea7c7313482e1ac0d2c9d35369adcd79bab970f
NwekeFavour <buchijunior90@gmail.com>
2025-06-30 13:06:35 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Routes, Route} from "react-router-dom"
+import Home from './pages/home'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         {/* routes */}
+        <Routes>
+          <Route path='/' exact element={<Home/>}/>
+        </Routes>
       </div>
     </>
   )
